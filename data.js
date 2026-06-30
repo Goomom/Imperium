b2aedf848816aad0b0bb52103e06b895931cf819
Gabriel <guaxinot@gmail.com>
2026-06-30 17:34:39 -0300

    Imperium

---
+++ data.js
@@ -0,0 +1,50 @@
+export const produtos = [
+    {
+        id: 0,
+        nome: 'Mercedes C63',
+        img: './assets/images/be7f9b05-c08b-4826-ac28-e2a94ceffef6.jpg',
+        tag: 'Carros',
+        descricao: 'Sedã esportivo de alto desempenho com motor V8 biturbo e acabamento premium.',
+        preco: 550000
+    },
+    {
+        id: 1,
+        nome: 'Pagani Huayra BC',
+        img: './assets/images/1755365114494107277230498371117.jpg',
+        tag: 'Carros',
+        descricao: 'Hipercarro exclusivo com construção em fibra de carbono e desempenho extremo.',
+        preco: 9000000
+    },
+    {
+        id: 2,
+        nome: 'Porsche 911 GT3',
+        img: './assets/images/17828244260461181505845672100280.jpg',
+        tag: 'Carros',
+        descricao: 'Esportivo com motor aspirado e dinâmica excepcional para pista e rua.',
+        preco: 1450000
+    },
+    {
+        id: 3,
+        nome: 'Ferrari SF90 Stradale',
+        img: './assets/images/ferrari-sf90.jpeg',
+        tag: 'Carros',
+        descricao: 'Superesportivo híbrido com tecnologia de Fórmula 1.',
+        preco: 7000000
+    },
+    {
+        id: 4,
+        nome: 'Lamborghini Aventador SVJ',
+        img: './assets/images/aventador-svj.jpeg',
+        tag: 'Carros',
+        descricao: 'Ícone italiano equipado com motor V12 naturalmente aspirado.',
+        preco: 8500000
+    },
+    {
+        id: 5,
+        nome: 'McLaren 720S',
+        img: './assets/images/mclaren-720s.jpeg',
+        tag: 'Carros',
+        descricao: 'Supercarro leve com motor V8 biturbo e desempenho impressionante.',
+        preco: 3200000
+    }
+];
